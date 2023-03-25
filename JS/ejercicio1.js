70735feb76e5ec6ad90b4e9f34d2aab28fb15181
Rodrigo Vizcarra <rodrigovizcarra1996@gmail.com>
2023-03-25 05:22:44 -0300

    Ejercicio 1 completado

---
+++ JS/ejercicio1.js
@@ -0,0 +1,10 @@
+const auto = {
+    color: "Rojo",
+    marca: "Renault",
+    modelo: "Kangoo",
+    encender: function(){console.log("Auto encendido")},
+    apagar: function(){console.log("Auto apagado")}
+}
+
+auto.encender();
+auto.apagar();
