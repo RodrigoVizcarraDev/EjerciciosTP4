f738b343f3f2b9ae763741be01b00efa96126ca1
Rodrigo Vizcarra <rodrigovizcarra1996@gmail.com>
2023-03-26 21:05:46 -0300

    Ejercicio 2 comenzando

---
+++ JS/ejerciciosTrello/ejercicio_2.js
@@ -0,0 +1,7 @@
+// Crear un objeto calculadora, con las propiedades y métodos necesarios para realizar las operaciones de suma, resta, multiplicación y división. Mostrar por pantalla 2 operaciones con cada método.
+
+class Calculadora{
+    constructor(){
+        
+    }
+}
